ac8087af1dbfb97a243fb5602cff30a3e2e2eec3
DeJeremy <degraeuwe.jeremy@outlook.com>
2018-05-13 17:45:17 +0200

    Fusion des main des deux devoirs

--- src/homework.c
+++ src/homework.c
@@ -198,3 +198,134 @@ double *femIterativeSolverEliminate(femIterativeSolver *mySolver)
     return(X);
 }
 
+double femGrainsContactIterate(femGrains *myGrains, double dt, int iter)  
+{
+    int     n           = myGrains->n,
+            i,
+            j,
+            iContact;
+    double  *x          = myGrains->x,
+            *y          = myGrains->y,
+            *m          = myGrains->m,
+            *r          = myGrains->r,
+            *vy         = myGrains->vy,
+            *vx         = myGrains->vx,
+            *dvBoundary = myGrains->dvBoundary,
+            *dvContacts = myGrains->dvContacts,
+            rIn         = myGrains->radiusIn,
+            rOut        = myGrains->radiusOut,
+            error       = 0.0,
+            gap,rr, rx, ry, nx, ny, vn, dv, dvIn, dvOut;
+
+    error = 0;
+    iContact = 0;
+    for(i=0 ; i<n ; i++) 
+    {
+        for(j=i+1 ; j<n ; j++) 
+        { 
+            rx = (x[j]-x[i]);
+            ry = (y[j]-y[i]);
+            rr = sqrt(rx*rx+ry*ry);
+            nx = rx/rr;
+            ny = ry/rr;
+            if (iter == 0) 
+            {
+                  dv = dvContacts[iContact]; 
+            }
+            else 
+            {
+                  vn = (vx[i]-vx[j])*nx + (vy[i]-vy[j])*ny ;
+                  gap = rr - (r[i]+r[j]);
+                  dv = fmax(0.0, vn + dvContacts[iContact] - gap/dt);
+                  dv = dv - dvContacts[iContact];                      
+                  dvContacts[iContact] += dv; 
+                  error = fmax(fabs(dv),error); 
+            }
+            vx[i] -= dv * nx * m[j] / ( m[i] + m[j] );
+            vy[i] -= dv * ny * m[j] / ( m[i] + m[j] );
+            vx[j] += dv * nx * m[i] / ( m[i] + m[j] );
+            vy[j] += dv * ny * m[i] / ( m[i] + m[j] );                  
+            iContact++; 
+        }
+    }
+
+    for(i=0 ; i<n ; i++) 
+    {
+        rr = sqrt(x[i] * x[i] + y[i] * y[i]);      
+        nx = x[i] / rr;
+        ny = y[i] / rr;
+        if (iter == 0) 
+        {
+            dv = dvBoundary[i]; 
+        }
+        else 
+        {
+            vn = vx[i]*nx + vy[i]*ny ;      
+            gap = rOut - rr - r[i];
+            dvOut = fmax(0.0, vn + dvBoundary[i] - gap/dt);
+            gap = rr - rIn - r[i];
+            dvIn  = fmax(0.0,- vn - dvBoundary[i] - gap/dt);
+            dv = dvOut - dvIn - dvBoundary[i]; 
+            dvBoundary[i] += dv; 
+            error = fmax(fabs(dv),error);
+        }
+        vx[i] -= dv * nx;
+        vy[i] -= dv * ny;
+    }  
+    return error;
+}
+
+void femGrainsUpdate(femGrains *myGrains, double dt, double tol, double iterMax)
+{
+    int     i,
+            n           = myGrains->n,
+            iter        = 0;
+    double  *x          = myGrains->x,
+            *y          = myGrains->y,
+            *m          = myGrains->m,
+            *vy         = myGrains->vy,
+            *vx         = myGrains->vx,
+            gamma       = myGrains->gamma,
+            gx          = myGrains->gravity[0],
+            gy          = myGrains->gravity[1],
+            error;
+
+// 
+// -1- Calcul des nouvelles vitesses des grains sur base de la gravite et de la trainee
+//
+
+    for(i=0 ; i<n ; i++) 
+    {
+        //double fx = m[i] * gx - gamma * vx[i]; // fx = m[i] * gx - (gamma * (vx[i] - u(x[i],y[i]))
+        //double fy = m[i] * gy - gamma * vy[i]; // fy = m[i] * gy - (gamma * (vy[i] - v(x[i],y[i]))
+        // ========== tests ==========
+        double fx = m[i] * gx - gamma * vx[i];
+        double fy = m[i] * gy - gamma * vy[i];
+        // ==========      ===========
+        vx[i] += fx * dt / m[i];
+        vy[i] += fy * dt / m[i];  
+    }
+
+//
+// -2- Correction des vitesses pour tenir compte des contacts        
+//       
+           
+    do 
+    {
+        error = femGrainsContactIterate(myGrains,dt,iter);
+        iter++; 
+    }
+    while ((error > tol/dt && iter < iterMax ) || iter == 1);
+    printf("iterations = %4d : error = %14.7e \n",iter-1,error);
+ 
+//  
+// -3- Calcul des nouvelles positions sans penetrations de points entre eux
+//
+
+    for (i=0 ; i<n ; i++) 
+    {
+        x[i] += vx[i] * dt;
+        y[i] += vy[i] * dt; 
+    }
+}
+
--- src/main.c
+++ src/main.c
@@ -15,27 +15,41 @@
 
 int main(void)
 {  
-    printf("\n\n    V : Plot results \n");
-    printf("    S : Spy matrix \n");
-    printf("    F-B-I : Full solver - Band solver - Iterative solver \n");
-    printf("    X-Y-N : Renumbering along x - along y - No renumbering \n");
-    
-    femSolverType solverType = FEM_BAND;
-    femRenumType  renumType  = FEM_NO;
 
-    // chargement du maillage
+    // Initialisation des grains
+    int n = 45;
+    double radius    = 0.05;
+    double mass      = 0.1;
+    double radiusIn  = 0.5;
+    double radiusOut = 2.0;    
+    double dt      = 1e-2;
+    double tEnd    = 8.0;
+    double tol     = 1e-6;
+    double t       = 0;
+    double iterMax = 100;
+    femGrains* theGrains = femGrainsCreateSimple(n,radius,mass,radiusIn,radiusOut);
+
+    // Initialisation de la vitesse d'exécution des mouvements des grains
+    int theRunningMode = 1.0;
+    float theVelocityFactor = 0.005;
+
+    // Solveur bande en numérotation selon x
+    femSolverType solverType = FEM_BAND;
+    femRenumType  renumType  = FEM_XNUM;
+
+    // Chargement du maillage
     char meshFileName[] = "../data/meca1120-projet-meshMedium.txt";   
     
-    // initialisation du problème
+    // Initialisation du problème
     femDiffusionProblem* theProblem = femDiffusionCreate(meshFileName,solverType,renumType);
 
 
     clock_t tic = clock();
 
-    // résolution du problème
+    // Résolution du problème
     femDiffusionCompute(theProblem);
 
-    // print des infos sur le maillage - inutile dans le projet
+    // Print des infos sur le maillage - inutile dans le projet
     femSolverPrintInfos(theProblem->solver);
 
 
@@ -45,28 +59,63 @@ int main(void)
     
 
     int option = 1;
-    // Récupération si changemetn de type de solveur
+    // Récupération si changement de type de solveur
     femSolverType newSolverType = solverType;
     femRenumType  newRenumType  = renumType;
 
-    GLFWwindow* window = glfemInit("MECA1120 : homework 5 ");
+    GLFWwindow* window = glfemInit("MECA1120 : Projet 2018");
     glfwMakeContextCurrent(window);
 
     do 
     {
-        int testConvergence,w,h;
-        char theMessage[256];
-        sprintf(theMessage, "Max : %.4f ",femMax(theProblem->soluce,theProblem->size));
+        int testConvergence,w,h,i;
+        double currentTime = glfwGetTime();
+        char theMessage1[256],theMessage2[256];
+        sprintf(theMessage1, "Max : %.4f ",femMax(theProblem->soluce,theProblem->size));
+        sprintf(theMessage2, "Time : %g sec",t);
         glfwGetFramebufferSize(window,&w,&h);
       
         if (option == 1) {
             glfemReshapeWindows(theProblem->mesh,w,h);
-            glfemPlotField(theProblem->mesh,theProblem->soluce);   }
+            glfemPlotField(theProblem->mesh,theProblem->soluce);   
+        }
         else {
             glColor3f(1.0,0.0,0.0);
-            glfemPlotSolver(theProblem->solver,theProblem->size,w,h); }
-        glColor3f(0.0,0.0,0.0); glfemDrawMessage(20,460,theMessage);              
+            glfemPlotSolver(theProblem->solver,theProblem->size,w,h); 
+        }
+
+        for (i=0 ;i < theGrains->n; i++) {     
+            glColor3f(1,0,0); 
+            glfemDrawDisk(theGrains->x[i],theGrains->y[i],theGrains->r[i]); 
+        }       
+        glColor3f(0,0,0); glfemDrawCircle(0,0,radiusOut);
+        glColor3f(0,0,0); glfemDrawCircle(0,0,radiusIn);
+
+        glColor3f(0.0,0.0,0.0); glfemDrawMessage(20,460,theMessage1);
+        glColor3f(0.0,0.0,0.0); glfemDrawMessage(100,460,theMessage2);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        if (t < tEnd && theRunningMode == 1) {
+            printf("Time = %4g : ",t);  
+  //
+  // A decommenter pour pouvoir progresser pas par pas
+  //          printf("press CR to compute the next time step >>");
+  //          char c= getchar();
+  //
+            femGrainsUpdate(theGrains,dt,tol,iterMax);
+            t += dt; 
+        }
+
+        while ( glfwGetTime()-currentTime < theVelocityFactor ) {
+          if (glfwGetKey(window,'R') == GLFW_PRESS) 
+                theRunningMode = 1; 
+          if (glfwGetKey(window,'S') == GLFW_PRESS) 
+                theRunningMode = 0; 
+        }   
       
+      /*
         if (solverType != newSolverType || renumType != newRenumType) { 
             solverType = newSolverType;
             renumType = newRenumType;
@@ -94,11 +143,11 @@ int main(void)
         if (glfwGetKey(window,'X') == GLFW_PRESS)   newRenumType  = FEM_XNUM; 
         if (glfwGetKey(window,'Y') == GLFW_PRESS)   newRenumType  = FEM_YNUM; 
         if (glfwGetKey(window,'N') == GLFW_PRESS)   newRenumType  = FEM_NO; 
-       
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    */
+
     } while( glfwGetKey(window,GLFW_KEY_ESCAPE) != GLFW_PRESS &&
              glfwWindowShouldClose(window) != 1 );
+
             
     // Check if the ESC key was pressed or the window was closed
                
